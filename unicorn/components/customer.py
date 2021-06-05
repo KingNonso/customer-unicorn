1c569dfefc750ab8a487fa5834a60836e00583fc
Chinonso Ani <achinonso@gmail.com>
2021-06-05 17:57:31 +0100

    more readable error messages with comments on implementation

--- unicorn/components/customer.py
+++ unicorn/components/customer.py
@@ -11,8 +11,6 @@ class CustomerView(UnicornView):
     email = ""
     subscription = ""
     subscriptions = []
-    # customers = Customer.objects.all()
-    # customer: Customer = {}
     customers = []
 
     def mount(self):
@@ -20,18 +18,17 @@ class CustomerView(UnicornView):
         self.subscriptions = [s[0] for s in SUBSCRIPTION_TYPE]
 
     def save(self):
-        if self.is_valid():
-            # self.customers.append(self.customer)
-            print(self.name, self.email)
+        if self.is_valid():  # checks with the form class for server side validation
+            # print(self.name, self.email)
             customer = Customer(name=self.name, email=self.email, subscription=self.subscription)
             customer.save()
             self.customers = Customer.objects.all()
-            messages.success(self.request, "Customer was created")
+            messages.success(self.request, "Customer was created successfully")
             self.name = ''
             self.email = ''
             self.subscription = ''
         else:
             messages.error(self.request, "There was a problem with your last request")
 
-    def update(self, idx: int):
+    def update(self, idx: int):  # not implemented
         self.customers[idx].save()
